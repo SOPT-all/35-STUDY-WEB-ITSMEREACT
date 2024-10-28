ee2012758b1750ad4df0b004263fb896afcd0bcd
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-10-28 15:57:35 +0900

    feat: wweek1 실습 업로드

--- week1_KIMGEONHWI/src/App.jsx
+++ week1_KIMGEONHWI/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Greeting from "./components/Greeting";
+import Button from "./components/Button";
+import UserProfile from "./components/UserProfile";
+import Message from "./components/Message";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const user = { name: "Alice", age: 25 };
+  const handleClick = () => alert("Button clicked!");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Greeting name="Alice" />
+      <Button label="Primary" color="blue" onClick={handleClick} />
+      <Button label="Danger" color="red" onClick={handleClick} />
+      <UserProfile user={user} />
+      <Message isLoggedIn={true} />
+      <div style={{ padding: "20px" }}>
+        <Card>
+          <h2>Welcome</h2>
+          <p>This is a reusable card component.</p>
+        </Card>
+
+        <Card>
+          <button onClick={() => alert("Button clicked!")}>Click Me</button>
+        </Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ week1_KIMGEONHWI/src/components/UserProfile.jsx
@@ -0,0 +1,5 @@
+const UserProfile = ({ user }) => {
+  return <h1>{user.name}</h1>;
+};
+
+export default UserProfile;

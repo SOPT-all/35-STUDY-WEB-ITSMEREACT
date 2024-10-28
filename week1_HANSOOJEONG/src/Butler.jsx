80ed67954afc6d0bb1fc4bf0745b3aae6b48f4f3
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-10-28 14:39:42 +0900

    feat: 사료주기 useState

--- week1_HANSOOJEONG/src/Butler.jsx
+++ week1_HANSOOJEONG/src/Butler.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Butler = ({ name, age }) => {
-    return <p>{name}의 나이는 {age}살입니다.</p>;
+    return <p>{name} 집사님의 나이는 {age}살입니다.</p>;
 };
 
 export default Butler; 

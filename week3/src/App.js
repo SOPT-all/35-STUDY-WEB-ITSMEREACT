5e1999eb7eb7475e251b9fd0dfd3e1df6c8af411
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-11 01:39:10 +0900

    feat: 이벤트 핸들러- 버튼

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,29 +1,43 @@
-export default function App() {
+// //이벤트 핸들러 추가
+
+// export default function App() {
+//   return (
+//     <Toolbar
+//       onPlayMovie={() => alert('Playing!')}
+//       onUploadImage={() => alert('Uploading!')}
+//     />
+//   );
+// }
+
+// function Toolbar({onPlayMovie, onUploadImage}){
+//   return(
+//     <div>
+//       <Button onClick={onPlayMovie}>
+//         Play Movie
+//       </Button>
+//       <Button onClick={onUploadImage}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+// function Button({onClick, children}){
+//   return(
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+export default function Button() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
   return (
-    <Toolbar
-      onPlayMovie={() => alert('Playing!')}
-      onUploadImage={() => alert('Uploading!')}
-    />
-  );
-}
-
-function Toolbar({onPlayMovie, onUploadImage}){
-  return(
-    <div>
-      <Button onClick={onPlayMovie}>
-        Play Movie
-      </Button>
-      <Button onClick={onUploadImage}>
-        Upload Image
-      </Button>
-    </div>
-  );
-}
-
-function Button({onClick, children}){
-  return(
-    <button onClick={onClick}>
-      {children}
+    <button onClick={handleClick}>
+      Click me
     </button>
   );
 }

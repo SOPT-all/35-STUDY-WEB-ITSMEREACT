8fd339c873d0d0f3a7828f4ce74fe563a55c3cae
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-11-18 17:35:12 +0900

    feat: 객체 state 업데이트하기 실습

---
+++ week4_HANSOOJEONG/src/App.jsx
@@ -0,0 +1,12 @@
+import UpdateingObjects from "./components/UpdatingObjects"
+function App() {
+
+  return (
+    <>
+      <div>4주차 실습</div>
+      <UpdateingObjects/>
+    </>
+  )
+}
+
+export default App
